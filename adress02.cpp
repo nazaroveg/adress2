fe3c9ee88ad87c6370dc61092d4af1a689e931ea
nazaroveg <nazaroveg@yandex.ru>
2022-06-10 15:23:49 +0300

    уже лучше

--- adress02.cpp
+++ adress02.cpp
@@ -6,11 +6,12 @@
 #include <fstream>
 #include <Windows.h>
 
+
 class address
 {
 public:
+	address (){city, street, house, apartment;}
 	
-
 	std::string get_output_address(std::string ci, std::string st, int hou, int apart)
 	{
 		std::string a = std::to_string(hou);
@@ -18,33 +19,40 @@ public:
 		std::string out_add = (ci + ", " + st + ", " + a + ", " + b);
 		return out_add;
 	}
-	void sort(std::string s_arr, int size)
+	
+	void set(std::string ci, std::string st, int hou, int apart)
 	{
-		std::string* arr_2 = new std::string[size]();
+		city = ci, street = st, house = hou; apartment = apart;
+	}
+	void sort(address *arr, int size )
+	{
+		
+
+		address* arr_2 = new address[size]();
 
 		for (int i = 0; i < size; ++i)
 		{
-			arr_2[s_arr[i]]++;
+			arr_2[arr[i]]++;
 		}
 		int k{};
 		for (int i = 0; i < size; i++) {
-			while (arr_2[i] != "\0") {
-				s_arr[k] = i;
+			while (arr_2[i] != ' ') {
+				arr[k] = i;
 				k++;
 				arr_2[i]--;
 			}
 		}
 
 		delete[]arr_2;
-
-
-
 	}
 
 
+	
+
+
 private:
-	std::string city, street;
-	int house, apartment;
+	std::string city = {0}, street = {0};
+	int house = 0; int apartment = 0;
 };
 
 
@@ -71,34 +79,27 @@ int main()
 	
 	
 	address* add = new address[size];
-	
+	address adr;
 	std::string a;
 	std::string b;
 	int c;
 	int d;
-	std::string* result = new std::string[size];
+	
 
 	for (int i = 0; i < size; ++i)
 	{
 
-		/*fin >> a;
+		fin >> a;
 		fin >> b;
 		fin >> c;
-		fin >> d;*/
-		add[i] = getline(fin);
+		fin >> d;
+		add[i].set(a, b, c, d);	
 
-		result[i] = add->get_output_address(a, b, c, d);
 	}
-	add->sort(result, size);
-
-	
 	fin.close();
+	adr.sort(add, size);
 	
-	
-	
-		std::cout << result << std::endl;
-		
-	
+
 
 }
 
